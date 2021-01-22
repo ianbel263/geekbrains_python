5e5289aefd2ba2b1b14a681203720293003e2199
Andrey Belyaev <ianbel263@gmail.com>
2021-01-22 12:19:40 +0300

    Упрощает код

--- lesson_3/task_5.py
+++ lesson_3/task_5.py
@@ -5,23 +5,17 @@
 # Если специальный символ введен после нескольких чисел, то вначале нужно добавить сумму этих чисел к
 # полученной ранее сумме и после этого завершить программу.
 
-def remove_not_int(data):
-    """Удаляет все элементы списка, которые нельзя привести к int"""
+def replace_not_int(data):
+    """Заменяет нулями все элементы, которые нельзя привести к int"""
     data_list = data.split()
-    is_not_int = False
 
     for i, el in enumerate(data_list):
         try:
             data_list[i] = int(el)
         except ValueError:
-            is_not_int = True
+            data_list[i] = 0
 
-    if is_not_int:
-        numbers = [datum for datum in data_list if type(datum) is int]
-    else:
-        numbers = data_list
-
-    return numbers
+    return data_list
 
 
 def check_exit_key(data):
@@ -34,7 +28,7 @@ is_exit_symbol_enter = False
 
 while not is_exit_symbol_enter:
     user_data = input('Введите строку чисел, разделенных пробелами (для выхода из программы введите "y"): ')
-    total += sum(remove_not_int(user_data))
+    total += sum(replace_not_int(user_data))
     is_exit_symbol_enter = check_exit_key(user_data)
 
     print('Сумма:', total)
