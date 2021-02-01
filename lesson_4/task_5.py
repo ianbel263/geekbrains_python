dfa23d3be9277a475c152641be6899cbdf101535
Andrey Belyaev <ianbel263@gmail.com>
2021-02-01 11:07:39 +0300

    Работа над ошибками

--- lesson_4/task_5.py
+++ lesson_4/task_5.py
@@ -6,9 +6,9 @@
 from functools import reduce
 
 
-def sum_elements(prev_el, el):
-    return prev_el + el
+def sum_elements(acc, el):
+    return acc * el
 
 
-numbers = [number for number in range(100, 1001)]
+numbers = [number for number in range(100, 1001) if number % 2 == 0]
 print(reduce(sum_elements, numbers))
