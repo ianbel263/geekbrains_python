fef0fc5cca9b01eb44a5bf453616ca14f4c108d5
Andrey Belyaev <ianbel263@gmail.com>
2021-02-07 17:42:12 +0300

    Реализует сложение матриц разных размеров

--- lesson_7/task_1.py
+++ lesson_7/task_1.py
@@ -10,16 +10,13 @@
 
 from random import randint
 
-MATRIX_SIZE = 5
-LINE_SIZE = 6
-
 
 def render_matrix():
     matrix_list = []
-    for i in range(MATRIX_SIZE):
+    for i in range(randint(1, 10)):
         line = []
-        for j in range(LINE_SIZE):
-            line.append(randint(0, 20))
+        for j in range(randint(1, 10)):
+            line.append(randint(0, 100))
         matrix_list.append(line)
     return matrix_list
 
@@ -31,18 +28,32 @@ class Matrix:
     def __str__(self):
         result = ''
         for line in self.matrix_list:
-            string = f'{"     ".join(map(lambda x: f"{str(x).rjust(2)}", line))}\n'
+            string = f'{"     ".join(map(lambda x: f"{str(x).rjust(3)}", line))}\n'
             result += string
         return result
 
     def __add__(self, other):
         result = []
-        for i, line in enumerate(self.matrix_list):
+        big_matrix = self._sort_lists(self.matrix_list, other.matrix_list)[1]
+        for i, line in enumerate(big_matrix):
             new_line = []
-            for j, number in enumerate(line):
-                new_line.append(number + other.matrix_list[i][j])
+            try:
+                big_line = self._sort_lists(self.matrix_list[i], other.matrix_list[i])[1]
+                small_line = self._sort_lists(self.matrix_list[i], other.matrix_list[i])[0]
+            except IndexError:
+                result.append(big_matrix[i])
+                continue
+            for j, number in enumerate(big_line):
+                try:
+                    new_line.append(number + small_line[j])
+                except IndexError:
+                    new_line.append(number)
             result.append(new_line)
         return Matrix(result)
+
+    @staticmethod
+    def _sort_lists(list_1, list_2):
+        return sorted([list_1, list_2], key=len)
 
 
 matrix_1 = Matrix(render_matrix())
@@ -50,4 +61,3 @@ matrix_2 = Matrix(render_matrix())
 print('Исходная матрица №1: ', matrix_1, sep='\n')
 print('Исходная матрица №2: ', matrix_2, sep='\n')
 print('Полученная матрица: ', matrix_1 + matrix_2, sep='\n')
-# добавить варианты сложения разных матриц
