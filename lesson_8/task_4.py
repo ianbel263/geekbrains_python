d8f4a6346871950ba2fc541357f1014961654cdf
Andrey Belyaev <ianbel263@gmail.com>
2021-02-11 11:48:11 +0300

    Добавляет цвета текста

--- lesson_8/task_4.py
+++ lesson_8/task_4.py
@@ -13,11 +13,12 @@
 # изученных на уроках по ООП.
 
 TEXT_COLORS = {
-    'normal': '\033[37m',
-    'warning': '\031[37m',
-    'success_add': '\032[37m',
-    'success_issue': '\033[37m',
+    'normal': '\033[0m',
+    'warning': '\033[31m',
+    'success_add': '\033[32m',
+    'success_issue': '\033[33m',
 }
+
 
 class Warehouse:
     def __init__(self, name):
@@ -25,22 +26,24 @@ class Warehouse:
         self._products = {}
 
     def print_all_products(self):
+        print(TEXT_COLORS['normal'], 'ТОВАР НА СКЛАДЕ:', sep='')
         for key, value in self._products.items():
-            print(f'{key}:')
+            print(TEXT_COLORS['normal'], f'{key}:', sep='')
             for el in value:
-                print(el)
+                print(f'    {el}')
 
     def add_product(self, product, count: int):
         if not isinstance(count, int):
-            print(TEXT_COLORS['warning'] + 'Количество товара надо вводить только целыми числами')
+            print(TEXT_COLORS['warning'], 'Количество товара надо вводить только целыми числами\n', sep='')
             return
         if product.category in self._products.keys():
             for el in self._products[product.category]:
                 if el['article_number'] == product.article_number and el['name'] == product:
                     el['count'] += count
+                    print(TEXT_COLORS['success_add'], f'Количество товара увеличено на {count} единиц', sep='')
                     return
                 elif el['article_number'] == product.article_number and el['name'] != product:
-                    print(f'Неверно введен артикул товара: {product.name}')
+                    print(TEXT_COLORS['warning'], f'Неверно введен артикул товара: {product.name}', sep='')
                     print(f'артикулу: {product.article_number} соответствует товар:\n{el["name"]}')
                     print('Товар на склад не добавлен\n')
                     return
@@ -51,31 +54,37 @@ class Warehouse:
             'price': product.price,
             'count': count
         })
+        print(TEXT_COLORS['success_add'], 'Товар добавлен на склад', sep='')
 
     def issue_product(self, product, count: int):
         if product.category not in self._products.keys():
-            print('Такого товара на склвде нет\n')
+            print(TEXT_COLORS['warning'], 'Такого товара на склвде нет\n', sep='')
             return
 
         for el in self._products[product.category]:
             if el['article_number'] == product.article_number:
                 if el['count'] > count:
                     el['count'] -= count
+                    print(TEXT_COLORS['success_issue'], f'Количество товара уменьшено на {count} единиц', sep='')
                     break
                 elif el['count'] == count:
                     self._products[product.category].remove(el)
-                    print('Товар полностью удален со склада')
+                    print(TEXT_COLORS['success_issue'], 'Товар полностью удален со склада', sep='')
                     break
                 else:
-                    print('Такого количества товара на складе нет')
+                    print(TEXT_COLORS['warning'], 'Такого количества товара на складе нет', sep='')
                     print(f'Остаток товара: {el["count"]}')
                     break
             else:
-                print('Такого товара на склвде нет\n')
+                print(TEXT_COLORS['warning'], 'Такого товара на склвде нет\n', sep='')
 
 
 class Equipment:
-    def __init__(self, article_number, name, color, price):
+    def __init__(self, article_number, name, color, price: float):
+        if not isinstance(price, float) and not isinstance(price, int):
+            print(TEXT_COLORS['warning'], 'Цену товара надо вводить только числами, в том числе с точкой\n', sep='')
+            return
+
         self.article_number = article_number
         self.name = name
         self.color = color
@@ -113,13 +122,14 @@ printer_hp = Printer(2, 'HP', 'black', 10000, 15)
 printer_hp_2 = Printer(1, 'HP', 'black', 10000, 15)
 scanner_canon = Scanner(3, 'Canon', 'black', 5000, 'tablet')
 copier_xerox = Copier(4, 'Xerox', 'silver', 9000, True)
+copier_canon = Copier(5, 'Canon', 'white', '10000', True)
 
 warehouse_moscow.issue_product(printer_brother, 15)
 
 warehouse_moscow.add_product(printer_brother, 'sasa')
 warehouse_moscow.add_product(printer_brother, 10)
-warehouse_moscow.add_product(printer_brother, 10)
-warehouse_moscow.add_product(printer_brother, 10)
+warehouse_moscow.add_product(printer_brother, 14)
+warehouse_moscow.add_product(printer_brother, 28)
 warehouse_moscow.add_product(printer_hp, 10)
 warehouse_moscow.add_product(printer_hp_2, 10)
 warehouse_moscow.add_product(scanner_canon, 5)
@@ -127,11 +137,11 @@ warehouse_moscow.add_product(scanner_canon, 5)
 warehouse_moscow.add_product(printer_hp, 5)
 
 warehouse_moscow.print_all_products()
-print()
 warehouse_moscow.issue_product(printer_brother, 17)
 warehouse_moscow.issue_product(printer_brother, 9)
 warehouse_moscow.issue_product(printer_brother, 3)
 warehouse_moscow.issue_product(printer_brother, 1)
-warehouse_moscow.issue_product(printer_brother, 1)
-print()
+warehouse_moscow.issue_product(printer_brother, 10)
+warehouse_moscow.issue_product(printer_brother, 15)
+warehouse_moscow.issue_product(printer_brother, 12)
 warehouse_moscow.print_all_products()
